f7f5ede2c3bbfe63353f039f44df982484e6650a
pospi <pospi@spadgos.com>
2011-02-03 18:15:37 +1000

    Allow config file to be overridden in document root, to avoid the need to edit files in repository

--- includes.inc.php
+++ includes.inc.php
@@ -1,4 +1,10 @@
 <?php
-	require_once('config.inc.php');
+	// Load a config file from various places.
+	// This allows you to define your search script configurations on a per-vhost basis.
+	if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/grepstrap.conf.php')) {							// document root
+		include($_SERVER['DOCUMENT_ROOT'] . '/grepstrap.conf.php');
+	} else {																						// default config
+		include('grepstrap.conf.php');
+	}
 ?>
  
